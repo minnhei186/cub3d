4322c1dcbead516bb2af5e90545bec606057a778
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-01-05 14:04:45 +0900

    Fixes #7 reconstruct sturct

---
+++ project/src/game_init.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_init.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hosokawa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/26 15:09:43 by hosokawa          #+#    #+#             */
+/*   Updated: 2025/01/05 13:48:47 by hosokawa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "wall.h"
+
+void	wall_init(t_game *game)
+{
+	game->wall.mlx = mlx_init();
+	game->wall.win = mlx_new_window(game->wall.mlx, WIDTH, HEIGHT,
+			"wall wall!");
+	game->wall.img = mlx_new_image(game->wall.mlx, WIDTH, HEIGHT);
+	game->wall.addr_pt = mlx_get_data_addr(game->wall.img, &(game->wall.bpp),
+			&(game->wall.size_line), &(game->wall.endian));
+	game->wall.key_up = 0;
+	game->wall.key_down = 0;
+	game->wall.key_left = 0;
+	game->wall.key_right = 0;
+}
+
+void	camera_init(t_game *game)
+{
+	game->camera.pos_x = PLAYERX;
+	game->camera.pos_y = PLAYERY;
+	game->camera.dir_x = INITDIRX;
+	game->camera.dir_y = INITDIRY;
+	game->camera.plane_x = INITPLANEX;
+	game->camera.plane_y = INITPLANEY;
+	game->camera.step = 0.1; // デフォルトの移動速度を設定
+}
+
+void	ddaInfo_init(t_game *game)
+{
+	game->ddaInfo.mapX = 0;
+	game->ddaInfo.mapY = 0;
+	game->ddaInfo.sideDistX = 0;
+	game->ddaInfo.sideDistY = 0;
+	game->ddaInfo.deltaDistX = 0;
+	game->ddaInfo.deltaDistY = 0;
+	game->ddaInfo.perpWallDist = 0;
+	game->ddaInfo.stepX = 0;
+	game->ddaInfo.stepY = 0;
+	game->ddaInfo.hit = 0;
+	game->ddaInfo.side = 0;
+}
+
+void	worldMap_init(t_game *game)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			if (i == 0 || i == 9 || j == 0 || j == 9)
+				game->worldMap[i][j] = 1;
+			else
+				game->worldMap[i][j] = 0;
+			ft_printf("%i", game->worldMap[i][j]);
+			j++;
+		}
+		ft_printf("\n");
+		i++;
+	}
+}
--- project/src/wall.c
+++ project/src/wall.c
@@ -6,7 +6,7 @@
 /*   By: hosokawa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 14:52:17 by hosokawa          #+#    #+#             */
-/*   Updated: 2024/12/30 15:45:12 by hosokawa         ###   ########.fr       */
+/*   Updated: 2025/01/05 13:46:39 by hosokawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,18 @@ int	game_loop(t_game *game)
 
 	update_player(game);
 	clear_window(&game->wall);
-
-	//更新した位置を表示
-	//printf("pos_x: %2f\n",game->camera.pos_x);
-	//printf("pos_y: %2f\n",game->camera.pos_y);
-
-	// 再描画処理
 	pixel = 0;
 	while (pixel < WIDTH)
 	{
-		t_ddaInfo ddaInfo;
 
 		camera = 2.0 * (double)pixel / (double)WIDTH - 1.0;
 		rayX = game->camera.dir_x + (game->camera.plane_x * camera);
 		rayY = game->camera.dir_y + (game->camera.plane_y * camera);
 		calculate_start_ddaInfo(rayX, rayY, game->camera.pos_x,
-			game->camera.pos_y, &ddaInfo);
-		calculate_dda_algo(game->worldMap, &ddaInfo);
-		calculate_perp_hight(&ddaInfo);
-		draw(pixel, ddaInfo.perpWallDist, &game->wall);//ここら辺は直接gameでもいいな
+			game->camera.pos_y, &game->ddaInfo);
+		calculate_dda_algo(game->worldMap, &game->ddaInfo);
+		calculate_perp_hight(&game->ddaInfo);
+		draw(pixel,game);
 		pixel++;
 	}
 	mlx_put_image_to_window(game->wall.mlx, game->wall.win, game->wall.img, 0,0);
@@ -52,6 +45,7 @@ int	main(void)
 
 	wall_init(&game);
 	camera_init(&game);
+	ddaInfo_init(&game);
 	worldMap_init(&game);
 	readKeys(&game);
 	mlx_loop_hook(game.wall.mlx, game_loop, &game);
