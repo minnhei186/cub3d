3b83e1b6cde1bce0ac991c7264f1553a5f136486
naopen <naokizssm@gmail.com>
2025-01-29 03:10:40 +0900

    Refactor: Improve map line parsing and player handling in `load_data_utils2.c`

--- project/src/load_data_utils2.c
+++ project/src/load_data_utils2.c
@@ -6,7 +6,7 @@
 /*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 03:05:15 by nkannan           #+#    #+#             */
-/*   Updated: 2025/01/29 03:06:23 by nkannan          ###   ########.fr       */
+/*   Updated: 2025/01/29 03:08:36 by nkannan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,15 +39,26 @@ int	is_map_line(const char *line, int i)
 	return (line[i] && (is_map_char(line[i]) || is_player_char(line[i])));
 }
 
-int	add_map_line(t_map_data *map_data, const char *line)
-{
-	char	*trimmed;
-	int		len;
-	int		j;
 
-	trimmed = ft_strtrim(line, " \n");
-	if (!trimmed)
+static int	handle_player(t_map_data *map_data, char *trimmed, int j)
+{
+	if (map_data->player_found)
+	{
+		ft_printf("Error: More than one player start found.\n");
 		return (-1);
+	}
+	map_data->player_dir = trimmed[j];
+	map_data->player_x = j;
+	map_data->player_y = map_data->map_height - 1;
+	map_data->player_found = 1;
+	trimmed[j] = '0';
+	return (0);
+}
+
+static int	allocate_and_store_map_line(t_map_data *map_data, char *trimmed)
+{
+	int len;
+
 	map_data->map_height++;
 	map_data->map = ft_realloc_double_ptr((void **)map_data->map, sizeof(char *)
 			* (map_data->map_height + 1));
@@ -57,26 +68,30 @@ int	add_map_line(t_map_data *map_data, const char *line)
 		return (-1);
 	}
 	map_data->map[map_data->map_height - 1] = trimmed;
-	{
-		len = ft_strlen(trimmed);
-		if (len > map_data->map_width)
-			map_data->map_width = len;
-	}
+	len = ft_strlen(trimmed);
+	if (len > map_data->map_width)
+		map_data->map_width = len;
+	return (0);
+
+}
+
+int	add_map_line(t_map_data *map_data, const char *line)
+{
+	char	*trimmed;
+	int		j;
+
+	trimmed = ft_strtrim(line, " \n");
+	if (!trimmed)
+		return (-1);
+	if (allocate_and_store_map_line(map_data, trimmed) < 0)
+		return (-1);
 	j = 0;
 	while (trimmed[j])
 	{
 		if (is_player_char(trimmed[j]))
 		{
-			if (map_data->player_found)
-			{
-				ft_printf("Error: More than one player start found.\n");
+			if (handle_player(map_data, trimmed, j) < 0)
 				return (-1);
-			}
-			map_data->player_dir = trimmed[j];
-			map_data->player_x = j;
-			map_data->player_y = map_data->map_height - 1;
-			map_data->player_found = 1;
-			trimmed[j] = '0';
 		}
 		j++;
 	}
