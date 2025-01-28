22ba6dd81ab250c42fc252d88e6213509732ec27
naopen <naokizssm@gmail.com>
2025-01-29 03:08:16 +0900

    Add: `load_data_utils2.c`

---
+++ project/src/load_data_utils2.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_data_utils2.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkannan <nkannan@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/29 03:05:15 by nkannan           #+#    #+#             */
+/*   Updated: 2025/01/29 03:06:23 by nkannan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/wall.h"
+
+void	skip_whitespace(const char *line, int *i)
+{
+	while (line[*i] == ' ' || line[*i] == '\t')
+		(*i)++;
+}
+
+void	remove_comment(char *line, int start_index)
+{
+	int	j;
+
+	j = 0;
+	while (line[start_index + j] != '\0')
+	{
+		if (line[start_index + j] == '#')
+		{
+			line[start_index + j] = '\0';
+			break ;
+		}
+		j++;
+	}
+}
+
+int	is_map_line(const char *line, int i)
+{
+	return (line[i] && (is_map_char(line[i]) || is_player_char(line[i])));
+}
+
+int	add_map_line(t_map_data *map_data, const char *line)
+{
+	char	*trimmed;
+	int		len;
+	int		j;
+
+	trimmed = ft_strtrim(line, " \n");
+	if (!trimmed)
+		return (-1);
+	map_data->map_height++;
+	map_data->map = ft_realloc_double_ptr((void **)map_data->map, sizeof(char *)
+			* (map_data->map_height + 1));
+	if (!map_data->map)
+	{
+		free(trimmed);
+		return (-1);
+	}
+	map_data->map[map_data->map_height - 1] = trimmed;
+	{
+		len = ft_strlen(trimmed);
+		if (len > map_data->map_width)
+			map_data->map_width = len;
+	}
+	j = 0;
+	while (trimmed[j])
+	{
+		if (is_player_char(trimmed[j]))
+		{
+			if (map_data->player_found)
+			{
+				ft_printf("Error: More than one player start found.\n");
+				return (-1);
+			}
+			map_data->player_dir = trimmed[j];
+			map_data->player_x = j;
+			map_data->player_y = map_data->map_height - 1;
+			map_data->player_found = 1;
+			trimmed[j] = '0';
+		}
+		j++;
+	}
+	return (0);
+}
