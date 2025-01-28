2312acf0b6800f3bfe63d3108af323cb5ed34ef2
daii_hosokawa <hosokawa@daii-hosokawanoMacBook-Air.local>
2025-01-28 12:39:03 +0900

    Closes #37:use plaer_info which is use_data in game

--- project/src/copy.c
+++ project/src/copy.c
@@ -6,7 +6,7 @@
 /*   By: hosokawa <hosokawa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 11:25:14 by hosokawa          #+#    #+#             */
-/*   Updated: 2025/01/26 13:21:14 by hosokawa         ###   ########.fr       */
+/*   Updated: 2025/01/28 12:33:28 by hosokawa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,14 @@ void copy_data(t_game *game,t_use_data *use_data)
 
 	game->texInfo.floor_color=use_data->floor_color;
 	game->texInfo.ceilling_color=use_data->ceilling_color;
+
+	game->camera.pos_x=use_data->player_y;
+	game->camera.pos_y=use_data->player_x;
+	game->camera.dir_x=use_data->player_dir_y;
+	game->camera.dir_y=use_data->player_dir_x;
+	game->camera.plane_x=use_data->player_plane_y;
+	game->camera.plane_y=use_data->player_plane_x;
+
 }
 
 
